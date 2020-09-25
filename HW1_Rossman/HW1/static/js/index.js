ad50ac5ed5c932d79012c22ee7099ec33b176f6b
Loi <loibucket@users.noreply.github.com>
2020-09-24 22:23:17 -0400

    hw1

--- HW1_Rossman/HW1/static/js/index.js
+++ HW1_Rossman/HW1/static/js/index.js
@@ -1,12 +1,3 @@
-//direct button to functions
 document.addEventListener("DOMContentLoaded", function () {
     console.log("DOM fully loaded and parsed")
-})
-
-$('#day_dropdown a').click(function () {
-    $('#day_select').html($(this).text())
-})
-
-$('#open_dropdown a').click(function () {
-    $('#open_select').html($(this).text())
 })
